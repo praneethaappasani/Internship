0be57973e9115f67a750b4d8c2b16d11a242cdaa
praneethaappasani <praneethaappasani@gmail.com>
2025-06-09 11:40:46 +0530

    message

---
+++ Stack.java
@@ -0,0 +1,38 @@
+class Human
+{
+	void name(String name)
+	{
+		System.out.println("praneetha");
+	}
+}
+class Student
+{	
+	void gender(int rollno,char section,String gender)
+
+	{
+		System.out.println("06");
+		System.out.println("D");
+		System.out.println("female");
+	}
+
+}
+class Devloper
+{
+	void college(String college)
+	{
+		System.out.println("vasavi");
+	}
+}
+class Stack
+{
+public static void main(String args[])
+{
+	Human hm=new Human();
+	Student sc=new Student();
+	Devloper dd=new Devloper();
+	hm.name("praneetha");
+	sc.gender(06,'D',"female");
+	dd.college("vasavi");
+	
+}
+}	
